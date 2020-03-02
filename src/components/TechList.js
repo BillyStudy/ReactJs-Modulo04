ee26afc2981a1528f9e362f7ab13cda5b0fefc9c
BillyCoding <gustavo.rocha.191066@hotmail.com>
2020-03-02 14:45:59 -0300

    Remove state itens

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -17,11 +17,20 @@ export default class TechList extends Component{
         this.setState({ newTech: ''});
     }
 
+    handleDelete = (tech) => {
+        this.setState({ techs: this.state.techs.filter(t => t !== tech)})
+    }
+
     render(){
         return(
             <form onSubmit={this.handleSubmit} className="formList">
                 <ul className="list">
-                    {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                    {this.state.techs.map(tech => 
+                        <li key={tech}>
+                            <button type="button" className="button" onClick={() => this.handleDelete(tech)} style={{backgroundColor: 'darkred', width: 30, height: 30, padding: 5, marginRight: 20, borderRadius: 1000}}>X</button>
+                            {tech}
+                        </li>  
+                    )}
                 </ul>
                 <input type="text" onChange={this.handleInputChange} maxLength={20} value={this.state.newTech} />
                 <button type="submit" className="button">Adicionar</button>
