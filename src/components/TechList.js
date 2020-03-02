b8885efc7341458120355a7dcda799c3dbe23109
BillyCoding <gustavo.rocha.191066@hotmail.com>
2020-03-02 14:33:41 -0300

    State and Immutability

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 export default class TechList extends Component{
     
     state = {
+        newTech:'',
         techs: ['NodeJS', 'ReactJS', 'React-Native']
     }
-    
+    handleInputChange = e => {
+        this.setState({ newTech: e.target.value });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({ techs: [ ...this.state.techs, this.state.newTech]});
+        this.setState({ newTech: ''});
+    }
+
     render(){
         return(
-            <ul>
-                <li>NodeJS</li>
-                <li>ReactJS</li>
-                <li>React-Native</li>
-            </ul>
+            <form onSubmit={this.handleSubmit} className="formList">
+                <ul className="list">
+                    {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                </ul>
+                <input type="text" onChange={this.handleInputChange} maxLength={20} value={this.state.newTech} />
+                <button type="submit" className="button">Adicionar</button>
+            </form>
         )
     }
 }
